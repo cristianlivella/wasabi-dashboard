b36e7b432ff28726698e1915101b132823a7ebca
Cristian Livella <cristian@cristianlivella.com>
2021-03-22 16:23:47 +0100

    Add .gitignore and config file

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+define('WASABI_ROOT_EMAIL', 'email@email.com');
+define('WASABI_USERNAME', 'username');
+define('WASABI_PASSWORD', 'password');
+
+define('BUCKETS_ALIASES', [
+    'bucket_with_a_long_and_ugly_name_QSX543TER54737120' => 'My favourite bucket',
+]);
